445a176118fef2e87972500fc5cb180ff7cb412c
coji <coji@techtalk.jp>
2023-02-12 15:51:54 +0900

    chakra-ui

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,9 +1,15 @@
+import { renderToString } from 'react-dom/server'
+import { CacheProvider } from '@emotion/react'
+import createEmotionServer from '@emotion/server/create-instance'
 import { PassThrough } from 'stream'
 import type { EntryContext } from '@remix-run/node'
 import { Response } from '@remix-run/node'
 import { RemixServer } from '@remix-run/react'
 import isbot from 'isbot'
 import { renderToPipeableStream } from 'react-dom/server'
+
+import { ServerStyleContext } from './context'
+import createEmotionCache from './createEmotionCache'
 
 const ABORT_DELAY = 5000
 
@@ -13,6 +19,19 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
+  const cache = createEmotionCache()
+  const { extractCriticalToChunks } = createEmotionServer(cache)
+
+  const html = renderToString(
+    <ServerStyleContext.Provider value={null}>
+      <CacheProvider value={cache}>
+        <RemixServer context={remixContext} url={request.url} />
+      </CacheProvider>
+    </ServerStyleContext.Provider>,
+  )
+
+  const chunks = extractCriticalToChunks(html)
+
   const callbackName = isbot(request.headers.get('user-agent'))
     ? 'onAllReady'
     : 'onShellReady'
@@ -21,7 +40,12 @@ export default function handleRequest(
     let didError = false
 
     const { pipe, abort } = renderToPipeableStream(
-      <RemixServer context={remixContext} url={request.url} />,
+      <ServerStyleContext.Provider value={chunks.styles}>
+        <CacheProvider value={cache}>
+          <RemixServer context={remixContext} url={request.url} />
+        </CacheProvider>
+      </ServerStyleContext.Provider>,
+
       {
         [callbackName]: () => {
           const body = new PassThrough()
